319d3c36ac60832a4e34787404136af72e9ca413
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-08-07 00:29:55 +0530

    setup: vite + react + tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <div className='flex flex-col items-center justify-center w-full h-screen bg-[#2b2b2b]'>
+      <p className='text-7xl text-orange-500 font-bold underline'>
+        Hello World
+      </p>
+    </div>
+  )
+}
+
+export default App
