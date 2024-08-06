b66c47fd204a6f5c8c803fe4b5c14e3b61fb01f1
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-08-07 01:53:50 +0530

    add: LazyLoadImage components to lazy load images

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,9 +1,14 @@
+import Img from "./Img";
 
 const ProductCard = ({ product }) => {
     return (
         <div className="flex flex-col justify-between max-w-sm
                bg-slate-600 p-4 rounded-2xl overflow-hidden shadow-lg box-shadow-1">
-            <img className="w-full rounded-2xl" src={product?.image} alt={product?.title} />
+            <Img
+                src={product?.image}
+                alt={product?.title}
+                className="w-full rounded-2xl"
+            />
 
             <div className="px-6 py-4">
                 <div className="font-semibold text-xl mb-2">
