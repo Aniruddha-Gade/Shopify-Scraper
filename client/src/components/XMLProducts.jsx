3ff6336817c2fde326280172e9f3a7f6d64b11ab
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-08-07 12:00:16 +0530

    add: list of example XML URLs for user to click and see results

--- client/src/components/XMLProducts.jsx
+++ client/src/components/XMLProducts.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from 'axios'
 import { PRODUCT_URL } from "../services/api";
 import ProductCard from "./ProductCard";
 // icons
 import { RiDownloadCloud2Fill } from "react-icons/ri";
 import companyIcon from '../assets/images/product.gif'
-import Img from "./Img";
 
 
 // get API url
@@ -21,12 +20,12 @@ const XMLProducts = () => {
 
 
     // fetch Products
-    const fetchProducts = async () => {
+    const fetchProducts = async (url) => {
         setLoading(true);
         setProducts([])
         try {
             // console.log("Calling backend to fetch products");
-            if (sitemapUrl) {
+            if (url) {
                 // const response = await axios.post(READ_XML_URL_API, { sitemapUrl });
                 const response = await axios.post(READ_AND_SUMMARIZE_URL_API, { sitemapUrl });
 
@@ -65,6 +64,38 @@ const XMLProducts = () => {
         </div>
     )
 
+    // examples of XML Url
+    const exampleXMLUrl = [
+        {
+            id: 0,
+            url: "https://skinny.buywithai.shop/sitemap_products_1.xml?from=8301017759977&to=8420664934633"
+        },
+        {
+            id: 1,
+            url: "https://www.skinvestment.store/sitemap_products_1.xml?from=8791014015309&to=8805425185101"
+        },
+        {
+            id: 2,
+            url: "https://plumgoodness.com/sitemap_products_1.xml?from=290339409&to=7740185378876"
+        },
+        {
+            id: 3,
+            url: "https://skindulge.in/sitemap_products_1.xml?from=8003642196244&to=9430137209108"
+        }
+    ]
+
+    // handle Example Click
+    const handleExampleClick = (url) => {
+        setSitemapUrl(url);
+    };
+
+    // useEffect to fetch products when sitemapUrl changes
+    useEffect(() => {
+        if (sitemapUrl) {
+            // setProducts([])
+            fetchProducts(sitemapUrl);
+        }
+    }, [sitemapUrl]);
 
 
     return (
@@ -80,21 +111,41 @@ const XMLProducts = () => {
                 </h1>
             </div>
 
-            <div className="w-full flex-center">
-                <input
-                    type="text"
-                    value={sitemapUrl}
-                    onChange={(e) => setSitemapUrl(e.target.value)}
-                    onKeyDown={handleKeyPress}
-                    className="p-2 m-4 h-14 w-1/2 border text-black border-gray-300 rounded-xl focus:outline-none  "
-                    placeholder="Enter product sitemap XML URL"
-                />
-                <button
-                    onClick={fetchProducts}
-                    className="p-4 h-14 flex gap-2 items-center font-semibold bg-green-500 hover:bg-green-600 text-white rounded"
-                >
-                    <RiDownloadCloud2Fill className="text-xl" /> Fetch Products
-                </button>
+            <div className="w-full flex-center flex-col">
+                <div className="w-full flex-center">
+                    <input
+                        type="text"
+                        value={sitemapUrl}
+                        onChange={(e) => setSitemapUrl(e.target.value)}
+                        onKeyDown={handleKeyPress}
+                        className="p-2 m-4 h-14 w-1/2 border text-black border-gray-300 rounded-xl focus:outline-none  "
+                        placeholder="Enter product sitemap XML URL"
+                    />
+
+                    <button
+                        onClick={() => fetchProducts(sitemapUrl)}
+                        className="p-4 h-14 group flex gap-2 items-center font-semibold bg-green-500 hover:scale-95 hover:bg-white hover:text-green-600 text-white rounded duration-300"
+                    >
+                        <RiDownloadCloud2Fill className="text-xl group-hover:text-green-500" /> Fetch Products
+                    </button>
+                </div>
+
+                <div className="text-white">
+                    <h3 className="text-xl font-semibold">Examples of XML Url, Try it...!</h3>
+                    <ul>
+                        {exampleXMLUrl.map((link, ind) => (
+                            <li
+                                key={link.url + link.id}
+                                onClick={() => handleExampleClick(link.url)}
+                                className="text-blue-500 hover:text-orange-500 duration-200 cursor-pointer"
+                            >
+                                <span>{ind + 1} . </span>
+                                {link.url}
+                            </li>
+                        ))}
+                    </ul>
+
+                </div>
             </div>
 
 
@@ -109,8 +160,8 @@ const XMLProducts = () => {
                         <LoadingProductCardSkeleton />
                     </div>
                 ) :
-                    // if NO data found
-                    products.length === 0 ? (
+                    // if NO data found and sitemapUrl is set
+                    sitemapUrl && products.length === 0 ? (
                         <div className="bg-slate-500 fle flex-col  w-1/2 h-40 p-10 rounded-2xl text-3xl flex-center  ">
                             <p className="font-semibold text-4xl text-green-500">No Data Found...!</p>
                             <p className="font-semibold text-2xl text-slate-700">Try another Url</p>
