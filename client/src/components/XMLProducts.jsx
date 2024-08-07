8bcc053e4357091469f9ee2fb3dd4cbc1712908c
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-08-07 13:00:39 +0530

    feat: highlight clicked example URL if it matches input URL

--- client/src/components/XMLProducts.jsx
+++ client/src/components/XMLProducts.jsx
@@ -139,7 +139,7 @@ const XMLProducts = () => {
                             <li
                                 key={link.url + link.id}
                                 onClick={() => handleExampleClick(link.url)}
-                                className="text-blue-500 hover:text-orange-500 duration-200 cursor-pointer"
+                                className={`text-blue-500 duration-200 cursor-pointer ${link.url === sitemapUrl ?'text-orange-700' :'hover:text-orange-500'}`}
                             >
                                 <span>{ind + 1} . </span>
                                 {link.url}
